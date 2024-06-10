7bf9b33a6acd9806a241e66f427a4b2dc76b310e
Ubuntu <ubuntu@ip-172-31-31-182.ec2.internal>
2024-06-10 06:34:25 +0000

    first commit

---
+++ shellscript.sh
@@ -0,0 +1,15 @@
+
+#!/bin/bash
+#
+######################################
+# author : dhanya
+# date : 10/06/2024
+# version: V1
+# script : containing details of resource usage
+
+set-e
+
+df -h 
+free -g
+nproc
+
